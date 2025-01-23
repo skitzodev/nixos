7c0c50cad8cebe06fd3edbaab45ca516d57af043
skitzodev <skitzo@baarda.xyz>
2025-01-23 22:37:11 +0100

    feat: starship formatting

--- shell/starship.nix
+++ shell/starship.nix
@@ -5,7 +5,7 @@
   settings = {
     command_timeout = 1300;
     scan_timeout = 50;
-    format = "$all$nix_shell$nodejs$lua$golang$rust$php$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
+    format = "$all$nix_shell$nodejs$rust$python$git_branch$git_commit$git_state$git_status\n$username$hostname$directory";
     character = {
       success_symbol = "[](bold green) ";
       error_symbol = "[✗](bold red) ";
