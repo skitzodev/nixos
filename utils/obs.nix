4b84c4b872a35706af63dcac3a63bd9b07c19d58
skitzodev <skitzo@baarda.xyz>
2025-01-24 03:51:11 +0100

    feat: obs

---
+++ utils/obs.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+{
+
+  environment.systemPackages = [
+  (pkgs.wrapOBS {
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  })
+];
+
+}
