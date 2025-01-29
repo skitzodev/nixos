8841932c3a5fe40f0f7ee5005bd83962533fd7fa
skitzodev <skitzo@baarda.xyz>
2025-01-29 09:03:21 +0100

    removed DRY due to podman update

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -19,7 +19,6 @@
       py = "python3";
       server = "python3 -m http.server 6969";
       ipa = "ip a | grep 192.168 | awk '{print $2}' | cut -d '/' -f 1";
-      dry = "docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_HOST=$DOCKER_HOST moncho/dry";
       upgrade = "sudo nixos-rebuild switch --flake ~/Documents/nixos/";
     };
     ohMyZsh = {
